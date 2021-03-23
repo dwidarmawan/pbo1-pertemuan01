79279d4910bdb3ba68a898d058a706f8c999cdcb
dwidarmawan <dwi.darmawan@gmail.com>
2021-03-23 02:08:12 -0700

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
  public static void main (String [] args){
-  system.out.println("Hello World !!");
+  System.out.println("Hello Git !!");
+  System.out.println("Hello World !!");
  }
 }
